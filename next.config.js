e824989fd537046a1be3e42a99ab045cdf34fea3
sv456rty <sv456rty@gmail.com>
2023-06-27 12:36:18 -0700

    cache images to minimize load time

--- next.config.js
+++ next.config.js
@@ -3,6 +3,22 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   experimental: { optimizeCss: true },
+
+  // https://stackoverflow.com/questions/62077589/setting-cache-control-header-for-static-file-serving-on-nextjs-default-server
+  async headers() {
+    return [
+      {
+        source: "/:all*(svg|jpg|png)",
+        locale: false,
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=9999999999, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
