041c9b21cfa236ed932af8469dd3bdac9d9f9583
sv456rty <sv456rty@gmail.com>
2023-07-02 18:37:51 -0700

    adds back image cache control fin next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,22 @@ const nextConfig = {
   swcMinify: true,
   experimental: { optimizeCss: true },
 
+  // https://stackoverflow.com/questions/62077589/setting-cache-control-header-for-static-file-serving-on-nextjs-default-server
+  async headers() {
+    return [
+      {
+        source: "/:all*(svg|jpg|png|ico)",
+        locale: false,
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=9999999999, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
+
   modularizeImports: {
     "@mui/material/?(((\\w*)?/?)*)": {
       transform: "@mui/material/{{ matches.[1] }}/{{member}}",
